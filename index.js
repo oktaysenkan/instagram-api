1978a604ab2ae6f7337cd21ff41750d2d299a3f9
Oktay Şenkan <oktay.senkan@gmail.com>
2020-01-10 08:31:59 +0300

    User service implemented.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+import UserService from './src/Service/UserService'
 const request = require('request');
 require('dotenv').config()
 
 const userId = 301557358;
-
 const headers = {
   'Content-Type': 'application/json',
   'Cookie': `sessionid=${process.env.SESSION_ID};`
 };
-
 const options = {
   url: `https://www.instagram.com/graphql/query/?query_hash=52a36e788a02a3c612742ed5146f1676&variables={"reel_ids":["${userId}"],"precomposed_overlay":false}`,
   method: 'GET',
   headers: headers
 };
 
-request(options, (error, response, body) => {
-  console.log(body);
-  console.log(response.statusCode);
+UserService.getUserIdByUsername('oktaysenkan').then((userId => {
+  console.log(userId);
+  request(options, (error, response, body) => {
+    console.log(body);
+    console.log(response.statusCode);
+  });
+})).catch(error => {
+  console.log(error);
 });
+
+
---
+++ src/Service/UserService.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+class UserService {
+  static  getUserIdByUsername = (username) => {
+    return new Promise((resolve, reject) => {
+      request(`https://www.instagram.com/web/search/topsearch/?context=user&count=0&query=${username}`, (error, response, body) => {
+        const {users} = JSON.parse(body);
+        if (users.length > 0) {
+          resolve(users[0].user.pk);
+        } else {
+          reject('User cannot found!');
+        }
+      });
+    })
+  }
+}
+
+export default UserService;
