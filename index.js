d85e5923e478cfd29171e983921cf8839e5179a8
Oktay Şenkan <oktay.senkan@gmail.com>
2020-01-10 07:09:50 +0300

    Init commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+const request = require('request');
+require('dotenv').config()
+
+const userId = 301557358;
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Cookie': `sessionid=${process.env.SESSION_ID};`
+};
+
+const options = {
+  url: `https://www.instagram.com/graphql/query/?query_hash=52a36e788a02a3c612742ed5146f1676&variables={"reel_ids":["${userId}"],"precomposed_overlay":false}`,
+  method: 'GET',
+  headers: headers
+};
+
+request(options, (error, response, body) => {
+  console.log(body);
+  console.log(response.statusCode);
+});
